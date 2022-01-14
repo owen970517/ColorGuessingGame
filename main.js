1ad5b624b8ec43641b68301e11721829d1e2046e
Owen <tamia123123@naver.com>
2022-01-14 18:27:56 +0900

    update

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ const newColor = document.querySelector('.playAgain');
 //const g = Math.floor(Math.random() * 255) + 1;
 //const b = Math.floor(Math.random() * 255) + 1;
 var colors = generateRandomColor(6);
-//var pickedColor = colors[Math.floor(Math.random()*6)];
+//var chooseColor = colors[Math.floor(Math.random()*6)];
 var boxCount = 6;
 
 
@@ -19,7 +19,7 @@ easyBtn.addEventListener('click' , ()=> {
     playBtn.innerHTML="Playing easy"
     boxCount=3;
     colors = generateRandomColor(boxCount);
-    pickedColor = colors[Math.floor(Math.random()*3)];
+    chooseColor = colors[Math.floor(Math.random()*3)];
     for (var i=0; i < boxes.length; i++) {
         if(colors[i]) {
             boxes[i].style.background =colors[i];
@@ -27,8 +27,8 @@ easyBtn.addEventListener('click' , ()=> {
             boxes[i].style.display = 'none';
         }
     }
-    getRGB.textContent = pickedColor;
-    document.querySelector('h1').style.background = pickedColor;
+    getRGB.textContent = chooseColor;
+    document.querySelector('h1').style.background = chooseColor;
 })
 hardBtn.addEventListener('click' , ()=> {
     hardBtn.classList.toggle('active');
@@ -36,34 +36,34 @@ hardBtn.addEventListener('click' , ()=> {
     playBtn.innerHTML="Playing Hard"
     boxCount=6;
     colors = generateRandomColor(boxCount);
-    pickedColor = colors[Math.floor(Math.random()*6)];
+    chooseColor = colors[Math.floor(Math.random()*6)];
     for (var i=0; i < boxes.length; i++) {
         boxes[i].style.background =colors[i];
         boxes[i].style.display = 'block';
         
     }
-    getRGB.textContent = pickedColor;
-    document.querySelector('h1').style.background = pickedColor;
+    getRGB.textContent = chooseColor;
+    document.querySelector('h1').style.background = chooseColor;
 });
 
 playBtn.addEventListener('click' , ()=> {
     playBtn.textContent = "Playing...";
     playBtn.classList.toggle('active');
     colors = generateRandomColor(boxCount);
-    pickedColor = colors[Math.floor(Math.random()*boxCount)];
-    getRGB.textContent = pickedColor;
-    document.querySelector('h1').style.background = pickedColor;
+    chooseColor = colors[Math.floor(Math.random()*boxCount)];
+    getRGB.textContent = chooseColor;
+    document.querySelector('h1').style.background = chooseColor;
     for (var i =0; i<boxes.length; i++) {
         boxes[i].style.background = colors[i];
     }
 
 });
 newColor.addEventListener('click' , ()=> {
-    playBtn.textContent = "Playing..."
+    //playBtn.textContent = "Playing..."
     colors = generateRandomColor(boxCount);
-    pickedColor = colors[Math.floor(Math.random()*boxCount)];
-    getRGB.textContent = pickedColor;
-    document.querySelector('h1').style.background = pickedColor;
+    chooseColor = colors[Math.floor(Math.random()*boxCount)];
+    getRGB.textContent = chooseColor;
+    document.querySelector('h1').style.background = chooseColor;
     for (var i =0; i<boxes.length; i++) {
         boxes[i].style.background = colors[i];
     }
@@ -73,20 +73,20 @@ for(var i=0; i<colors.length; i++) {
     boxes[i].addEventListener('click' , function() {
         var selectedColor = this.style.background;
         if(selectedColor === document.querySelector('h1').style.background) {
-            win();
+            correct();
         } else {
-            lose(this);
+            wrong(this);
         }
         
     });
 }
-function win() {
+function correct() {
     for(var i=0; i<colors.length; i++) {
-        boxes[i].style.background = pickedColor;
+        boxes[i].style.background = chooseColor;
     }
     playBtn.innerHTML = "Correct!!!";
 }
-function lose(e) {
+function wrong(e) {
     e.style.background = "aquamarine";
     playBtn.innerHTML = "Try Again!!!";
 }
